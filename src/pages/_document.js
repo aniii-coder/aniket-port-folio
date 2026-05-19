6b310680d61249536a0b711123f80773bc4d58a7
Aniket Singh <aksingh22356@gmail.com>
2026-05-19 22:55:53 +0530

    added few skills

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="https://api.dicebear.com/9.x/thumbs/svg?seed=user-8f3a2c" />
+      </Head>
       <body>
         <Main />
         <NextScript />
